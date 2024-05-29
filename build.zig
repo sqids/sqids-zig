307b9a7cc920d043d39323395ddb986286636e58
pwnker <tilen.okretic@gmail.com>
2024-05-29 23:45:30 +0200

    [build.zig] fix: `source_file` -> `root_source_file` and specifying paths with `b.path`

--- build.zig
+++ build.zig
@@ -4,16 +4,16 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const sqids_module = b.addModule("sqids", .{ .source_file = .{ .path = "src/main.zig" } });
+    const sqids_module = b.addModule("sqids", .{ .root_source_file = b.path("src/main.zig") });
 
     const tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/tests/tests.zig" },
+        .root_source_file = b.path("src/tests/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const run_main_tests = b.addRunArtifact(tests);
+    tests.root_module.addImport("sqids", sqids_module);
 
-    tests.addModule("sqids", sqids_module);
+    const run_main_tests = b.addRunArtifact(tests);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
