02c1405c9b4bac8d776b235d813f576837a4c4fb
Louis Vignoli <louis.vignoli@pasqal.com>
2025-08-20 15:00:31 +0200

    Draft blocklist object

---
+++ src/main.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const sqids = @import("sqids");
+const mem = std.mem;
+
+const Sqids = sqids.Sqids;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const numbers = &.{ 1, 2, 3 };
+
+    const opts = sqids.Options{
+        // .blocklist = new_blocklist(allocator, alphabet, blocked_words),
+        // .alphabet = alphabet,
+    };
+
+    // Using the default Sqids, encode the numbers to a Sqids ID.
+    const s = try Sqids.init(opts);
+    defer s.deinit();
+    const id = try s.encode(numbers);
+    defer allocator.free(id);
+
+    // Print to stdout.
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{s}\n", .{id});
+}
