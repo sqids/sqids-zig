0fb0b9da8e8492b1348f493de88dade03dc39d74
Louis Vignoli <louis.vignoli@pasqal.com>
2025-08-20 15:00:31 +0200

    Upgrade build system to 0.14
    This is in preparation of 0.15, where the deprecated root_source_file field is removed
    from std.Build.ExecutableOptions and friends.
    
    In the process, I understood a bit more how things work, and properly run _all_ tests,
    counting the ones in root.zig as well. They should probably be moved these ones.

--- build.zig
+++ build.zig
@@ -4,17 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const sqids_module = b.addModule("sqids", .{ .root_source_file = b.path("src/root.zig") });
+    const sqids_module = b.addModule("sqids", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const tests = b.addTest(.{
+    const test_module = b.createModule(.{
         .root_source_file = b.path("src/tests/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
-    tests.root_module.addImport("sqids", sqids_module);
+    test_module.addImport("sqids", sqids_module);
 
-    const run_main_tests = b.addRunArtifact(tests);
+    const tests = b.addTest(.{ .root_module = test_module });
+    const root_tests = b.addTest(.{ .root_module = sqids_module });
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
+    test_step.dependOn(&b.addRunArtifact(tests).step);
+    test_step.dependOn(&b.addRunArtifact(root_tests).step);
 }
