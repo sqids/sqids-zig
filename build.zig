b067137e6867c00b9905254ef5be426a7d89e467
Louis Vignoli <louis.vignoli@pasqal.com>
2025-08-21 18:17:32 +0200

    Add Karl Seguin's test runners

--- build.zig
+++ build.zig
@@ -17,8 +17,14 @@ pub fn build(b: *std.Build) void {
     });
     test_module.addImport("sqids", sqids_module);
 
-    const tests = b.addTest(.{ .root_module = test_module });
-    const root_tests = b.addTest(.{ .root_module = sqids_module });
+    const tests = b.addTest(.{
+        .root_module = test_module,
+        .test_runner = .{ .path = b.path("src/tests/test_runner.zig"), .mode = .simple },
+    });
+    const root_tests = b.addTest(.{
+        .root_module = sqids_module,
+        .test_runner = .{ .path = b.path("src/tests/test_runner.zig"), .mode = .simple },
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&b.addRunArtifact(tests).step);
